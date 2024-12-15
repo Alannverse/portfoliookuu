43ad74d8a81d1659e61e9f3d954b686b265dbad1
Alannverse <alanpraseptialan@gmail.com>
2024-12-15 22:26:58 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Specify where your HTML and JS files are located
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
