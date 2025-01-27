eaeaa42275686124043a9527e30f7b60b7819013
Alannverse <alanpraseptialan@gmail.com>
2025-01-27 09:34:45 +0800

    Fix Tailwind CSS output

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   content: [
-    "./*.html", // Semua file HTML di root direktori
-    "./src/**/*.css", // Semua file CSS di folder src
-    "./dist/img/**/*", // Semua gambar di dist/img (jika Anda ingin menggunakan kelas Tailwind di atribut HTML)
+    "./*.html", // Semua file HTML di root
+    "./src/**/*.css,js", // Semua file CSS di src
   ],
   theme: {
     extend: {},
